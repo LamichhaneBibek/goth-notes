aa13b007f3f0cbb6194afbf41af5bf4940d94071
LamichhaneBibek <lamichhane.kebib@gmail.com>
2024-02-14 18:27:35 +0545

    refactor: Create render function for handling Templ components
    This commit introduces the `render` function in `cmd/render.go`, which acts as a wrapper for the `templ.Handler` function. The purpose of this function is to simplify rendering Templ components within a Fiber web server context. It provides a clean and consistent way to handle Templ components and their rendering options.

---
+++ cmd/render.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+)
+
+// render is a function serving as a wrapper for the templ.Handler function.
+func render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
+	componentHandler := templ.Handler(component)
+	for _, o := range options {
+		o(componentHandler)
+	}
+	return adaptor.HTTPHandler(componentHandler)(c)
+}
