cb67934d95041a756bdac9f0c8135356999abd68
LamichhaneBibek <lamichhane.kebib@gmail.com>
2024-02-16 08:40:30 +0545

    Add Notfound Handling and Templating Middleware in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,12 @@ import (
 	"github.com/LamichhaneBibek/goth-notes/notes"
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
 
 func main() {
 	app := fiber.New()
 
-	// app.Use(func(ctx *fiber.Ctx) error {
-	// 	return render(ctx, components.Notfound())
-	// })
 	notes.InitializeNotes()
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
@@ -35,16 +33,28 @@ func main() {
 	})
 	app.Delete("/note/:id", func(ctx *fiber.Ctx) error {
 		idStr := ctx.Params("id")
-
 		id, err := strconv.Atoi(idStr)
-
+		if err != nil {
+			ctx.Status(400)
+		}
 		err = notes.Delete(id)
-
 		if err != nil {
 			ctx.Status(404)
 		}
 		var notes templ.Component = components.Notes(notes.GetAll())
 		return render(ctx, notes)
 	})
+	app.Use(func(ctx *fiber.Ctx) error {
+		return render(ctx, components.Notfound())
+	})
 	app.Listen(":8080")
+}
+
+// render is a function serving as a wrapper for the templ.Handler function.
+func render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
+	componentHandler := templ.Handler(component)
+	for _, o := range options {
+		o(componentHandler)
+	}
+	return adaptor.HTTPHandler(componentHandler)(c)
 }
